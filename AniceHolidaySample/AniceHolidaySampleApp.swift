5c947ee822f5e702b2821dbd2fe526f28673cbcf
Syuan <s.yihsuan0201@gmail.com>
2024-06-20 18:35:36 +0800

    Initial Commit

---
+++ AniceHolidaySample/AniceHolidaySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniceHolidaySampleApp.swift
+//  AniceHolidaySample
+//
+//  Created by So í-hian on 2024/6/20.
+//
+
+import SwiftUI
+
+@main
+struct AniceHolidaySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
